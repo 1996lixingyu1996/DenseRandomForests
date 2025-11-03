f477dc8aafe866ba4a0bf44cf3e3ad494bd5052c
Xingyu Li <46596546+1996lixingyu1996@users.noreply.github.com>
2025-11-03 10:20:00 -0600

    Add files via upload

---
+++ src/splitCustom.h
@@ -0,0 +1,20 @@
+/*
+ vvvvvvvv External Constants Below -- Do Not Change vvvvvvvv
+ */
+
+#define LEFT      0x01
+#define RIGHT     0x02
+
+#define CLAS_FAM     0
+#define REGR_FAM     1
+#define SURV_FAM     2
+#define CRSK_FAM     3
+
+/*
+ ^^^^^^^^ External Constants Above -- Do Not Change ^^^^^^^^
+ */
+
+
+/*
+ vvvvvvvv Do Not Touch These Delarations Below vvvvvvvv
+ */
